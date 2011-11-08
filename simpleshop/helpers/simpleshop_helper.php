a12468db05fd03c8a37c88e92aa4ede1e91738c4
Joseph Wynn <wildlyinaccurate@gmail.com>
2011-11-08 16:51:18 +0000

    Committing a save-state

---
+++ simpleshop/helpers/simpleshop_helper.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * SimpleShop Helper
+ *
+ * Various helper functions specific to the SimpleShop Module
+ *
+ * @author  Joseph Wynn
+ * @since   1.0
+ */
+
+
+/**
+ *
+ */

120f2455703cc488d4b5a42d0d610e656f09b3d0
Joseph Wynn <wildlyinaccurate@gmail.com>
2011-11-21 22:11:29 +0000

    Updated proxies

--- simpleshop/models/Proxies/EntityCategoryProxy.php
+++ simpleshop/models/Proxies/EntityCategoryProxy.php
@@ -66,7 +66,7 @@ class EntityCategoryProxy extends \Entity\Category implements \Doctrine\ORM\Prox
         return parent::getImageId();
     }
 
-    public function addProduct($product)
+    public function addProduct(\Entity\Product $product)
     {
         $this->__load();
         return parent::addProduct($product);
@@ -153,7 +153,7 @@ class EntityCategoryProxy extends \Entity\Category implements \Doctrine\ORM\Prox
 
     public function __sleep()
     {
-        return array('__isInitialized__', 'id', 'title', 'slug', 'description', 'image_id', 'products', 'parent_category', 'child_categories', 'created_date', 'modified_date');
+        return array('__isInitialized__', 'id', 'title', 'slug', 'description', 'image_id', 'created_date', 'modified_date', 'products', 'parent_category', 'child_categories');
     }
 
     public function __clone()

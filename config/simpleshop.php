edf44528ecaf77b9cb905c26b6aba5c5e7f1782e
Joseph Wynn <joe@chromaagency.com>
2012-03-29 18:51:37 +0100

    Add Simpleshop config

---
+++ config/simpleshop.php
@@ -0,0 +1,7 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Simpleshop Configuration
+ */
+
+$config['simpleshop_breadcrumb_separator'] = '<span class="separator">&raquo;</span>';

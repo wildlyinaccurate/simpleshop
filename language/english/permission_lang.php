2e120256122fe8b57e523a6d497adb84237304a2
Joseph Wynn <wildlyinaccurate@gmail.com>
2012-11-30 18:38:10 +0000

    Add language lines for roles

--- language/english/permission_lang.php
+++ language/english/permission_lang.php
@@ -1,3 +1,8 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['simpleshop.role_create_edit_category'] = 'Create & edit categories';
+$lang['simpleshop.role_create_category']      = 'Create categories';
+$lang['simpleshop.role_edit_category']        = 'Edit categories';
+$lang['simpleshop.role_delete_category']      = 'Delete categories';
+$lang['simpleshop.role_create_product']       = 'Create products';
+$lang['simpleshop.role_edit_product']         = 'Edit products';
+$lang['simpleshop.role_delete_product']       = 'Delete products';
